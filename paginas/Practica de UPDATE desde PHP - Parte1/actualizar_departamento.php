1f5a0efc5599e96dacaeac5e6be941adc869fa6d
jony-diaz14 <jony.j.diaz.14@gmail.com>
2022-10-27 08:27:18 -0500

    practica sesion 12

---
+++ paginas/Practica de UPDATE desde PHP - Parte1/actualizar_departamento.php
@@ -0,0 +1,136 @@
+<?php
+    // Insertamos el código PHP donde nos conectamos a la base de datos 
+    require_once "conexion.php";
+	
+	//Recuperamos los valores de las cajas de texto y de los demás objetos de formulario
+	//Como los valores vienen desde un FORMULARIO web, se debe usar la supervariable de PHP -- $_POST[ ];
+    $numero_dpto = $_POST["txt_id_departamento_oculto"];
+	$departamento = strtoupper(trim($_POST["txt_departamento"])); //Se convierte a MAYUSCULAS usando la función  strtoupper()
+
+    // Escribimos la consulta para ACTUALIZAR LOS DATOS EN LA TABLA de departamentos (PDO)
+	// Debido a que en esta tabla los 2 campos son de tipo STRING se encierran en '' las 2 variables de PHP
+	// Al ser una sentecia UPDATE de SQL es OBLIGATORIO usar la sentecia WHERE apuntando al campo Llave Primario
+       $sqlUPDATE = "UPDATE departamentos SET descripcion = '$departamento' WHERE departamento='$numero_dpto'";
+	   
+    // Ejecutamos la sentencia UPDATE de SQL a partir de la conexión usando PDO 
+	// mediante la propiedad "EXEC" de la linea de conexión ***************************
+	
+        $conn->exec($sqlUPDATE);
+	    $mensaje = "DEPARTAMENTO ACTUALIZADO SATISFACTORIAMENTE";
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Actualización de departamentos desde PHP hacia MySQL</title>
+
+<style type="text/css" media="screen">
+
+body { background-color:#999;}
+
+#wrapper {
+	margin: auto;
+	width: 960px;
+	height: 390px;
+	background-color: #CCC;
+	}
+	
+#caja1 {
+	width: 300px;
+	height: 50px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja2 {
+	width: 300px;
+	height: 50px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja3 {
+	width: 300px;
+	height: 50px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja4 {
+	width: 940px;
+	height: 300px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 40px;
+	background-color: #333;
+	clear: both;
+
+	position: relative;
+	top: 10px;
+	}
+	
+#imagen1 { position:relative; top:10px; right:-10px; }
+
+#texto1 {
+	width: 500px;
+	height: 270px;
+	margin-left: 5px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #CCC;
+	padding: 5px;
+	float: right;
+	right: -10px;
+	top: 10px;
+	}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+   <div id="caja1">Licenciatura en Tecnologías de la Información</div>
+   <div id="caja2">Programación web</div>
+   <div id="caja3">Formulario para modificar departamentos en la base de datos desde una página web</div>
+ 
+   <div id="caja4">
+     <div id="texto1"><br>
+      <p></p>
+ 
+        <fieldset style="width: 90%;"    >
+            <legend><?php echo ($mensaje); ?></legend>
+                <div>
+                    <br />
+                         <b>Código de Departamento:</b> <?php echo ($numero_dpto); ?>
+                    <br />
+					<br />
+                         <b>Número de Departamento:</b> <?php echo ($departamento); ?>
+                    <br />
+					<br />
+                    <a href="alta_departamentos.php">REGISTRAR OTRO DEPARTAMENTO</a>
+				    <br />
+					<br />
+                    <a href="reporte_editar_departamentos.php">REPORTE GENERAL DE DEPARTAMENTOS</a>
+                </div>
+        </fieldset> 
+     </div>
+  </div>
+</div>
+     <?php
+			//Cerramos la conexion a la base de datos *************************************
+			$conn = null;
+     ?>
+</body>
+</html>
---
+++ paginas/Practica de UPDATE desde PHP - Parte1/reporte_editar_departamentos.php
@@ -0,0 +1,169 @@
+<?php
+    //Insertamos el código PHP donde nos conectamos a la base de datos *******************************
+    require_once "conexion.php";
+    // Escribimos la consulta para recuperar los registros de la tabla de MySQL
+    $sql = 'SELECT departamento, descripcion FROM departamentos';
+
+    // Ejecutamos la consulta y asignamos el resultado a la variable llamada $result
+    $result = $conn->query($sql);
+      
+    // Recuperamos los valores o registros de la variable $result y los asignamos a la variable $rows
+    $rows = $result->fetchAll();
+	
+	// Los valores que tendrá la variable $rows se organizan en un arreglo asociativo
+	// (Variable con varias valores)
+	// y se usará un ciclo foreach para recuper los valores uno a uno de ese arreglo
+    // El resultado se mostrará en una tabla HTML ****************************************************
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Regitro de registros a eliminar</title>
+
+<style type="text/css" media="screen">
+
+body { background-color:#999;}
+
+#wrapper {
+	margin: auto;
+	width: 960px;
+	height:auto;
+	background-color: #CCC;
+	}
+	
+#caja1 {
+	width: 300px;
+	height: 60px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja2 {
+	width: 300px;
+	height: 60px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja3 {
+	width: 300px;
+	height: 60px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #FFC;
+	float: left;
+}
+
+#caja4 {
+	width: 940px;
+	height:400px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-top: 40px;
+	background-color: #333;
+	clear: both;
+	/*
+		 position:absolute; 
+		 top:200px;
+    */
+		 
+	position: relative;
+	top: 10px;
+	}
+	
+#imagen1 { position:relative; top:10px; right:-10px; }
+
+#texto1 {
+	width: 900px;
+	margin-left: 5px;
+	margin-right: 10px;
+	margin-top: 10px;
+	background-color: #CCC;
+	padding: 5px;
+	float: right;
+	right: -10px;
+	top: 10px;
+	}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+   <div id="caja1">Licenciatura en Tecnolog&iacute;as de la Informaci&oacute;n</div>
+   <div id="caja2">Programaci&oacute;n web</div>
+   <div id="caja3">Reporte de registros de una tabla para ser ACTUALIZADOS en l&iacute;nea (PHP con PDP y MySQL)</div>
+ 
+   <div id="caja4">
+     <div id="texto1"><br>
+ 
+        <table border="1" style="width:100%;">
+        <thead>
+            <tr>
+                <th>Codigo Dpto</th>
+                <th>Departamento</th>
+                <th>Eliminar</th>
+                <th>Editar</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+        <?php
+            foreach ($rows as $row) {
+			//Imprimimos en la página un renglon de tabla HTML por cada registro de tabla de MySQL
+        ?>
+            <tr>
+			    <!-- En estas celdas de HTML se muestran los valores recuperados de la consulta SELECT de SQL -->
+				<!-- En esta parte mostrarás tus propios campos de tu TABLA CATALOGO (todos los que conforman tu tabla) -->
+                <td><?php echo $row['departamento']; ?></td>
+                <td><?php echo $row['descripcion']; ?></td>
+
+                <!-- CELDA 1 para la ilga de BORRAR -->
+                <td><a href="eliminar_departamentos.php?id=<?php echo $row['departamento']; ?>">
+				       eliminar
+                    </a>
+                </td>
+                
+                <!-- CELDA 2 para la ilga de EDITAR -->
+				<!-- Esta es la LIGA o LINK para enviar el valor del campo llave al archivo 2 de la práctica -->
+                 <td><a href="editar_departamentos.php?id=<?php echo $row['departamento']; ?>">
+				        editar
+                     </a>
+                </td>
+                    
+            </tr>
+			
+	    <!-- Aqui se cierra el ciclo foreach de PHP que se usó para mostrar en pantalla los valores de los campos de la consulta SQL tipo SELECT -->
+        <?php } ?>
+        
+         <tr>
+                <td colspan="4">&nbsp;</td>
+         </tr>
+         <tr>
+                <td>&nbsp;</td>
+                <td><a href="alta_departamentos.php">Agregar otro departamento</a></td>
+                <td>&nbsp;</td>
+                <td colspan="4">&nbsp;</td>
+         </tr>   
+        </tbody>
+    </table>
+     </div>
+  </div>
+</div>
+     <?php
+			//Cerramos la oonexion a la base de datos **********************************************
+			$conn = null;
+     ?>
+</body>
+</html>
